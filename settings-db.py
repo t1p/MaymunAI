853ad07c63a55d247cb2818f5aa81fa475b61c29
TIP <trapeznikovip@gmail.com>
2026-01-12 18:32:34 +0100

    Удален ключ OpenAI

--- settings-db.py
+++ settings-db.py
@@ -1,11 +1,8 @@
 # Database settings
 DB_CONFIG = {
-    "dbname": "maymunai",
+    "dbname": "postgesunai",
     "user": "maymun",
     "password": "ydYy9^&.q4#P9",
     "host": "localhost",
     "port": "5432"
 }
-
-# API keys
-OPENAI_API_KEY = "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" 
